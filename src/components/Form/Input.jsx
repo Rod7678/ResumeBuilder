6c452f1cb982d9b33928835d1f71848d3a2dd0bf
Rohit <rohitghadge348@gmail.com>
2025-11-23 23:45:26 +0530

    added user detail form

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -2,7 +2,7 @@ export default function Input({id, label, ...props}){
     return (
         <p className="control">
             <label htmlFor={id}>{label}</label>
-            <input type={id} name={id} required {...props} />
+            <input name={id} required {...props} />
         </p>
     )
 }
--- src/components/Form/UserForm.jsx
+++ src/components/Form/UserForm.jsx
@@ -1,10 +1,22 @@
 import Input from "./Input.jsx";
+import "./Form.css"
+import Button from "../UI/Button.jsx";
 
 export default function UserForm(){
     return (
     <form action="">
         <h2>Fill the details</h2>
-        <Input />
+        <div className="control-row">
+            <Input id="firstName" label="first name" type={Text} value="john"/>
+            <Input id="lastName" label="last name" type={Text} value="cena"/>
+        </div>
+        <Input id="fatherName" label="father name" type={Text} value="mena"/>
+        <Input id="email" label="Enter your email" type="email" value="mena"/>
+
+        <p className="form-actions">
+            <Button txtOnly={true}>Reset</Button>
+            <Button >submit</Button>
+        </p>
     </form>
     )
-}
+} 
