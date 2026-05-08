411fd35f0710fa80b911959079563b330fa3021e
Rohit <rohitghadge348@gmail.com>
2026-05-08 23:47:06 +0530

    Refactor Language component to integrate language fetching and updating; enhance form handling for language entries

--- frontend/src/components/Form/Language.jsx
+++ frontend/src/components/Form/Language.jsx
@@ -2,29 +2,90 @@ import { useNavigate } from "react-router-dom";
 import FormDiv from "../UI/FormDiv";
 import Input from "../UI/Input.jsx";
 import Button from "../UI/Button";
-import { SaveEducationDetails } from "../../utils/http.js";
+import {
+  fetchLatestResume,
+  SaveLanguageDetails,
+  UpdateLanguageDetails,
+} from "../../utils/http.js";
+import { useUser } from "../../context/UserContext.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useFormData } from "../../hooks/useFormData.js";
+import { useEffect } from "react";
 
 const Language = ({ onSelect }) => {
+  const initialState = {
+    lan: "",
+    additionalInfo: "",
+    languageLevel: "basic",
+  };
+  const { entryId } = useUser();
   const { mutate, isPending, isError, error } = useMutation({
-    mutationFn: SaveEducationDetails,
+    mutationFn: SaveLanguageDetails,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["latestResume"] });
+      onSelect();
     },
   });
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    const fd = new FormData(event.target);
-    const data = Object.fromEntries(fd.entries());
-    mutate(data);
-    onSelect();
-  };
-  
+  const {
+    mutate: updateLanguage,
+    isPending: updateIsPending,
+    isError: updateIsError,
+    error: updateError,
+  } = useMutation({
+    mutationFn: ({ data, entryId }) =>
+      UpdateLanguageDetails({ data, id: entryId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["latestResume"] });
+      onSelect();
+    },
+  });
+
+  const { data: languageData } = useQuery({
+    queryKey: ["latestResume"],
+    queryFn: fetchLatestResume,
+  });
+  let language = null;
+  if (entryId != null) {
+    language =
+      languageData?.languages?.find((lan) => lan.id === entryId) || null;
+  } else {
+    language = languageData?.languages?.[0] || null;
+  }
+
+  const {
+    formData,
+    setFormValues,
+    handleSubmit: handleFormSubmit,
+    handleChange: handleInputChange,
+  } = useFormData({
+    initialState,
+    onSubmit: (payload) => {
+      if (language) {
+        updateLanguage({ data: payload, entryId: language.id });
+      } else {
+        mutate(payload);
+      }
+    },
+  });
+
+  useEffect(() => {
+    if (language) {
+      setFormValues({
+        lan: language.lan || "",
+        additionalInfo: language.additional_info || "",
+        languageLevel: language.language_level || "basic",
+      });
+    }
+    return;
+  }, [language, setFormValues]);
   return (
     <FormDiv title="Languages" onSend={handleFormSubmit}>
       <Input
         id="lan"
         name="lan"
+        value={formData.lan}
+        onChange={handleInputChange}
         label="Language"
         type="text"
         placeholder="eg. Marathi"
@@ -32,12 +93,17 @@ const Language = ({ onSelect }) => {
       <Input
         name="additionalInfo"
         id="addInfo"
+        value={formData.additionalInfo}
+        onChange={handleInputChange}
+        label="Additional Information"
         type="textarea"
         placeholder="eg. I have proficient experience in marathi"
       />
-      <label htmlFor="languageLevel"></label>
+      <label htmlFor="languageLevel">Language Level</label>
       <select name="languageLevel" id="languageLevel">
-        <option value="basic">Basic</option>
+        <option value="basic" selected>
+          Basic
+        </option>
         <option value="conversational">Conversational</option>
         <option value="proficient">Proficient</option>
         <option value="fluent">Fluent</option>
