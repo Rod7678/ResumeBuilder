a788ffd1f7353e335e9a6637741c9e8a647a1d87
Rohit <rohitghadge348@gmail.com>
2026-05-31 22:40:47 +0530

    Enhance Skills component with mutation handling and form submission feedback

--- frontend/src/components/Form/Skills.jsx
+++ frontend/src/components/Form/Skills.jsx
@@ -2,53 +2,109 @@ import { useEffect, useState } from "react";
 import FormDiv from "../UI/FormDiv";
 import Input from "../UI/Input";
 import { useFormData } from "../../hooks/useFormData";
-import { useQuery } from "@tanstack/react-query";
-import { fetchLatestResume } from "../../utils/http";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  fetchLatestResume,
+  queryClient,
+  SaveSkillsDetails,
+  UpdateSkillsDetails,
+} from "../../utils/http";
 import { useUser } from "../../context/UserContext";
+import Button from "../UI/Button";
 
-const Skills = ({onSelect}) => {
+const Skills = ({ onSelect }) => {
   const initialState = {
     skill: "",
     skillInfo: "",
     proficiencyLevel: "basic",
   };
-  
-  const {entryId} = useUser();
-  
-  const {data: skillData} = useQuery({
+
+  const { entryId } = useUser();
+
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: SaveSkillsDetails,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["latestResume"] });
+      onSelect();
+    },
+  });
+
+  const {
+    mutate: updateData,
+    isError,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: ({ data, entryId }) =>
+      UpdateSkillsDetails({ data, id: entryId }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["latestResume"] });
+      onSelect();
+    },
+  });
+
+  const { data: skillData } = useQuery({
     queryFn: fetchLatestResume,
-    queryKey: ["latestResume"]
+    queryKey: ["latestResume"],
   });
   let skill = null;
-  if(entryId != null){
+  if (entryId != null) {
     skill = skillData?.skills?.find((s) => s.id === entryId) || null;
-  }else{
+  } else {
     skill = skillData?.skills?.[0] || null;
   }
-  const {formData, handleSubmit: handleFprmSubmit} = useFormData({initialState, onSubmit: (payload) => {
-    if(skill){
+  const {
+    formData,
+    handleSubmit: handleFormSubmit,
+    handleChange: handleInputChange,
+    setFormValues,
+  } = useFormData({
+    initialState,
+    onSubmit: (payload) => {
+      if (skill) {
+        updateData({ data: payload, entryId: skill.id });
+      } else {
+        mutate(payload);
+      }
+    },
+  });
+  useEffect(() => {
+    if (skill) {
+      setFormValues({
+        skill: skill.skill || "",
+        skillInfo: skill.skillInfo || "",
+        proficiencyLevel: skill.proficiencyLevel || "basic",
+      });
 
+      return;
     }
-  }})
-  // useEffect(()=>{
+  }, [skill]);
 
-  // }, [])
   return (
-    <FormDiv title={"Add Your skills"} onSend={onSelect}>
+    <FormDiv title={"Add Your skills"} onSend={handleFormSubmit}>
       <Input
         placeholder="Enter your skill"
         id={"skills"}
         name={"skills"}
+        value={formData.skill}
+        onChange={handleInputChange}
         type={"text"}
       />
       <Input
         placeholder="Information about your skill"
         id={"skillInfo"}
         name={"skillInfo"}
+        value={formData.skillInfo}
+        onChange={handleInputChange}
         type={"textarea"}
       />
       <label htmlFor="proficiencyLevel">Proficiency Level</label>
-      <select name="proficiencyLevel" id="proficiencyLevel">
+      <select
+        name="proficiencyLevel"
+        id="proficiencyLevel"
+        value={formData.proficiencyLevel}
+        onChange={handleInputChange}
+      >
         <option value="basic" selected>
           Basic
         </option>
@@ -57,9 +113,15 @@ const Skills = ({onSelect}) => {
         <option value="fluent">Fluent</option>
         <option value="natBil">Native/Bilingual</option>
       </select>
+      {isPending && <p>Form is submitting please wait</p>}
+      {!isPending && <Button className="mt-4">Done</Button>}
+      {isError && (
+        <p>
+          {error.info?.message || "An error occured during submitting form"}
+        </p>
+      )}
     </FormDiv>
   );
 };
-
 
 export default Skills;
