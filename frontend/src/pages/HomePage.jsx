6ecf07af57f60457effb7077c291eb4486cffea4
Rohit <rohitghadge348@gmail.com>
2026-06-02 23:57:30 +0530

    feat: add HomePage component with navigation, hero, feature, future, and templates sections

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+const HomePage = () => {
+  return (
+    <>
+      <section className="nav">
+        <h1>This is home page nav bar </h1>
+      </section>
+      <section className="hero">
+        <h2>Hero section</h2>
+      </section>
+      <section className="feature">
+        <h2>Feature section</h2>
+      </section>
+      <section className="future">
+        <h2>future sect</h2>
+      </section>
+      <section className="templates">
+        <h2>Template Sec</h2>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
