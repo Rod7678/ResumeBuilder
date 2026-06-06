e8f5f37ade5e0102032ae2348712179a3639a297
Rohit <rohitghadge348@gmail.com>
2026-06-06 23:43:27 +0530

    Update HeroSection to use dynamic class names for images and adjust layout

--- frontend/src/components/Sections/HeroSection.jsx
+++ frontend/src/components/Sections/HeroSection.jsx
@@ -3,21 +3,21 @@ import Button from "../UI/Button";
 import resume1 from "/resumeImg1.avif";
 
 const HeroSection = () => {
-    const imageArray = [
-        {id: 1,imgSrc : resume1},
-        {id: 2,imgSrc : resume1},
-        {id: 3,imgSrc : resume1},
-        {id: 4,imgSrc : resume1},
-        {id: 5,imgSrc : resume1},
-        {id: 6,imgSrc : resume1},
-        {id: 7,imgSrc : resume1},
-        {id: 8,imgSrc : resume1},
-        {id: 9,imgSrc : resume1},
-        {id: 10,imgSrc : resume1},
-    ];
+  const imageArray = [
+    { id: 1, imgSrc: resume1, className: "mt-0" },
+    { id: 2, imgSrc: resume1, className: "mt-40" },
+    { id: 3, imgSrc: resume1, className: "mt-60" },
+    { id: 4, imgSrc: resume1, className: "mt-40" },
+    { id: 5, imgSrc: resume1, className: "mt-0" },
+    // { id: 6, imgSrc: resume1, className: "mt-0" },
+    // { id: 7, imgSrc: resume1, className: "mt-[[-10rem]]" },
+    // { id: 8, imgSrc: resume1, className: "mt-0" },
+    // { id: 9, imgSrc: resume1, className: "mt-0" },
+    // { id: 10, imgSrc: resume1, className: "mt-0" },
+  ];
   return (
     <>
-      <section className="hero h-100 bg-neutral-400 flex items-center justify-center p-12 w-full">
+      <section className="hero h-fit bg-neutral-400 flex flex-col items-center justify-center p-12 w-full">
         <h1>Hero section</h1>
         <p>
           SubTitle Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -29,12 +29,16 @@ const HeroSection = () => {
           <Button>Get Started</Button>
           <Button>Templates</Button>
         </div>
-        <div className="grid grid-cols-5 gap-2">
-          <ul>{imageArray.map((image)=>{<div key={image.id} className="img-grid1">
-            <BoxShadow>
-              <img src={image.imgSrc} alt="Resume1" />
-            </BoxShadow>
-          </div>})}</ul>
+        <div className="heroSection-temps" >
+          <ul className="grid grid-cols-5 gap-2">
+            {imageArray.map((image) => (
+              <div key={image.id} className={`img-grid ${image.className}`}>
+                <BoxShadow>
+                  <img src={image.imgSrc} alt="Resume1" />
+                </BoxShadow>
+              </div>
+            ))}
+          </ul>
         </div>
       </section>
     </>
