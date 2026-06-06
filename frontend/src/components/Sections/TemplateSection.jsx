64fe431d48959509f8e0e52b3c1a53ba3c7b1b1d
Rohit <rohitghadge348@gmail.com>
2026-06-06 23:59:50 +0530

    Update TemplateSection layout and add resume image display

--- frontend/src/components/Sections/TemplateSection.jsx
+++ frontend/src/components/Sections/TemplateSection.jsx
@@ -1,21 +1,22 @@
 import BoxShadow from "../UI/BoxShadow";
+import resume1 from "/resumeImg1.avif";
 
 const TemplateSection = () => {
   return (
     <>
     
       {/* - in next time after creating templates i can make an list of array of object and then apply map for listing */}
-      <section className="templates h-100 bg-neutral-400 flex items-center justify-center p-12 w-full flex-col">
-        <h2 className="text-xl">Template Section</h2>
-        <div className="grid grid-cols-4 gap-4">
-          <BoxShadow className="h-20 p-10">a</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
-          <BoxShadow className="h-20 p-10">bd</BoxShadow>
+      <section className="templates h-fit bg-neutral-400 flex items-center justify-center p-12 w-full flex-col">
+        <h2 className="text-6xl font-medium">Template Section</h2>
+        <div className="grid grid-cols-4 py-4 gap-4">
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
+          <BoxShadow className="rounded-xl overflow-hidden object-fill"><button className="p-0"><img src={resume1} alt="" /></button></BoxShadow>
         </div>
       </section>
     </>
